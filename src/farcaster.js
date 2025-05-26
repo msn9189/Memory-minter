3f12943099782868cfdfdbe5d2c045b5742634cb
Nazi <mehdizadeh.sn@gmail.com>
2025-05-26 11:39:49 +0000

    farcaster.js created for Farcaster

---
+++ src/farcaster.js
@@ -0,0 +1,9 @@
+import { MiniApp } from "@farcaster/miniapps-sdk";
+
+const miniApp = new MiniApp({
+  appId: "Memory-minter",
+  displayName: "Memory App",
+  description: "Write and mint your memories on Base",
+});
+
+export default miniApp;
